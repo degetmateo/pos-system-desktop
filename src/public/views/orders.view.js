ab120938412af9b779336dfef4f467e431b7fd49
degetmateo <degetmateo@gmail.com>
2026-01-29 13:01:59 -0300

    orders view update

--- src/public/views/orders.view.js
+++ src/public/views/orders.view.js
@@ -1,84 +1,76 @@
 import Navigation from "../components/navigation/navigation.js";
-import router from "../router.js";
 import GenericView from "./GenericView.js";
 
 export default class OrdersView extends GenericView {
     constructor () {
         super();
 
-        this.offset = 0;
-        this.continue = true;
-
         this.view = document.createElement('div');
         this.view.classList.add('view');
-
         this.view.append(new Navigation());
 
         this.container = document.createElement('div');
         this.container.classList.add('container', 'orders-view-container');
         this.view.append(this.container);
 
-        this.tableContainer = document.createElement('div');
-        this.tableContainer.classList.add('orders-view-table-container');
-        this.container.append(this.tableContainer);
+        this.container.innerHTML = `
+            <div 
+                class="orders-view-table-container"
+                style="
+                    flex: 1;
+                    width: 100%;
+                    overflow-y: scroll;
+                    height: calc(100vh - 80px);
+                "
+            >
+                <table class="orders-view-table">
+                    <thead>
+                        <tr class="orders-view-table-head-row">
+                            <th>NOMBRE</th>
+                            <th>CLIENTE</th>
+                            <th>TIPO</th>
+                            <th>MONTO</th>
+                            <th>FECHA</th>
+                            <th>FACTURA</th>
+                            <th>ESTADO</th>
+                        </tr>
+                    </thead>
+                    <tbody id="orders-view-table-body"></tbody>
+                </table>
+            </div>
 
-        this.table = document.createElement('table');
-        this.table.classList.add('orders-view-table');
-        this.tableContainer.append(this.table);
-
-        this.head = document.createElement('thead');
-        this.table.append(this.head);
-
-        this.head.innerHTML = `
-            <tr>
-                <th>Número</th>
-                <th>Cliente</th>
-                <th>Tipo</th>
-                <th>Monto</th>
-                <th>Fecha</th>
-                <th>Factura</th>
-                <th>Estado</th>
-            </tr>
+            <div class="orders-view-buttons-container">
+                <button
+                    type="button"
+                    id="orders-view-button-previous"
+                    class="orders-view-button"
+                >Anterior</button>
+                <button
+                    type="button"
+                    id="orders-view-button-next"
+                    class="orders-view-button"
+                >Siguiente</button>
+            </div>
         `;
 
-        this.body = document.createElement('tbody');
-        this.table.append(this.body);
+        this.container.addEventListener('click', (event) => {
+            if (event.target.matches('#orders-view-button-previous')) {
+                this.previous();
+            };
 
-        this.buttonsContainer = document.createElement('div');
-        this.buttonsContainer.classList.add('orders-view-buttons-container');
-        this.container.append(this.buttonsContainer);
-
-        this.buttonPrevious = document.createElement('button');
-        this.buttonPrevious.classList.add('orders-view-button');
-        this.buttonPrevious.textContent = 'Anterior';
-        this.buttonPrevious.type = 'button';
-        this.buttonsContainer.append(this.buttonPrevious);
-
-        this.buttonNext = document.createElement('button');
-        this.buttonNext.classList.add('orders-view-button');
-        this.buttonNext.textContent = 'Siguiente';
-        this.buttonNext.type = 'button';
-        this.buttonsContainer.append(this.buttonNext);
-
-        this.buttonPrevious.addEventListener('click', (event) => {
-            event.preventDefault();
-            this.previous();
+            if (event.target.matches('#orders-view-button-next')) {
+                this.next();
+            };
         });
 
-        this.buttonNext.addEventListener('click', (event) => {
-            event.preventDefault();
-            this.next();
-        });
+        this.offset = 0;
+        this.continue = true;
     };
 
     async init () {
         this.app.innerHTML = '';
         this.app.append(this.view);
-
-        this.clear_table();
-
-        const orders = await this.fetch_orders();
-        this.draw_orders(orders);
+        this.draw_orders(await this.fetch_orders());
     };
 
     async fetch_orders () {
@@ -88,37 +80,32 @@ export default class OrdersView extends GenericView {
             if (!request.ok) throw new Error(response.error.message);
             return response.data;
         } catch (error) {
-            console.error(error);  
+            console.error(error);
+            return [];
         };
     };
 
-    clear_table () {
-        this.body.innerHTML = '';
+    clear () {
+        document.querySelector('#orders-view-table-body').innerHTML = '';
     };
 
     draw_orders (orders) {
+        this.clear();
         for (const order of orders) {
-            const row = document.createElement('tr');
-
-            row.classList.add('orders-view-row');
-
             const date = new Date(order.created_at);
-
-            date.getDate();
-
-            row.innerHTML = `
-                <td>${order.number}</td>
-                <td>${order.customer ? order.customer.name : 'SIN ASIGNAR'}</td>
-                <td>${order.type === 'major' ? "MAYORISTA" : "MINORISTA"}</td>
-                <td>${order.total_price / 100}</td>
-                <td>${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}</td>
-                <td>
-                    <a href="/api/orders/receipt/${order.id}">Descargar Factura</a>
-                </td>
-                <td>${order.status}</td>
+            document.querySelector('#orders-view-table-body').innerHTML += `
+                <tr>
+                    <td id="${order.id}" class="order-view-table-row-td">${order.number}</td>
+                    <td id="${order.id}" class="order-view-table-row-td">${order.customer ? order.customer.name : 'SIN ASIGNAR'}</td>
+                    <td id="${order.id}" class="order-view-table-row-td">${order.type === 'major' ? "MAYORISTA" : "MINORISTA"}</td>
+                    <td id="${order.id}" class="order-view-table-row-td">${order.total_price / 100}</td>
+                    <td id="${order.id}" class="order-view-table-row-td">${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}</td>
+                    <td id="${order.id}" class="order-view-table-row-td">
+                        <a href="/api/orders/receipt/${order.id}">Descargar Factura</a>
+                    </td>
+                    <td id="${order.id}" class="order-view-table-row-td">${order.status.toUpperCase()}</td>
+                </tr>
             `;
-
-            this.body.append(row);
         };
     };
 
@@ -129,14 +116,11 @@ export default class OrdersView extends GenericView {
         this.continue = true;
 
         const orders = await this.fetch_orders();
-        
-        this.clear_table();
         this.draw_orders(orders);
     };
 
     async next () {
         if (!this.continue) return;
-
         this.offset += 20;
 
         const orders = await this.fetch_orders();
@@ -147,7 +131,6 @@ export default class OrdersView extends GenericView {
             return;
         };
 
-        this.clear_table();
         this.draw_orders(orders);
     };
 };
